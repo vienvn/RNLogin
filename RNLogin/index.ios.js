cbec58c1eb64720c27d7bb595741fd39d5f86ed6
Vien Van Nguyen <vien.nguyen@saigontechnology.vn>
2017-05-25 09:42:24 +0700

    Init code

---
+++ RNLogin/index.ios.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+import ReactNativeLogin from './App/components/App';
+import {
+  AppRegistry
+} from 'react-native';
+
+
+
+AppRegistry.registerComponent('RNLogin', () => ReactNativeLogin);
